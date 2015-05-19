988dd95a96e8f825971335148ae24ba6ba5edaab
jax1109 <jax1@rambler.ru>
2015-05-19 18:20:03 +0300

    Hello world message

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by kovalevskyi.i on 5/8/2015.
+ */
+public class HelloWorld {
+    public static void main(String... args) {
+        System.out.println("Hello " + args[0]);
+    }
+}
