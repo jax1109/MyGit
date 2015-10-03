e4ee8ce3a6b9fde9735d0bf5beb419a744816851
jax1109 <jax1@rambler.ru>
2015-10-04 01:27:36 +0300

    Delete HelloWorld.java

--- src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by kovalevskyi.i on 5/8/2015.
- */
-public class HelloWorld {
-    public static void main(String... args) {
-        System.out.println("Hello1 " + args[0]);
-    }
-}
